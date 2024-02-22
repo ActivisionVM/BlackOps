5c1b1765ca57a9daa145327a5e544ec93e7c1d00
Ángel Pérez Mengual <angel.perez.mengual@ces-vegamedia.es>
2024-02-22 19:14:38 +0100

    Creacion del ZombiCorredor

---
+++ Zombies/src/zombies/ZombiCorredor.java
@@ -0,0 +1,11 @@
+package zombies;
+
+public class ZombiCorredor extends Zombi {
+	
+	/**
+	 * Constructor del ZombiCorredor, establece la vida del ZombiCorredor a 80 y su daño a 15.
+	 */
+    public ZombiCorredor() {
+        super(80, 15);
+    }
+}
