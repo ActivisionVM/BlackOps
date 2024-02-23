11110c262447ccccf83aa7198c56fd9f0adffa59
Joaquin Lorca Nieto <joaquin.lorca.nieto@ces-vegamedia.es>
2024-02-23 11:40:55 +0100

    ZombiSaltarin

---
+++ Zombies/src/zombies/ZombiSaltarin.java
@@ -0,0 +1,10 @@
+package zombies;
+
+public class ZombiSaltarin extends Zombi{
+	/**
+	 * Constructor del ZombiCorredor, establece la vida del ZombiCorredor a 150 y su daño a 8.
+	 */
+	ZombiSaltarin(){
+		super(150, 8); // Vida y velocidad heredados como atributo de la clase padre de una funcion super.
+	}
+}
