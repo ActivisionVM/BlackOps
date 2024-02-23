75b8ab8f45c7339da7148179903fd62f8163533b
Juan Miguel López Bravo <juanmiguel.lopez.bravo@ces-vegamedia.es>
2024-02-23 11:48:49 +0100

    Creacion ZombiTanque

---
+++ Zombies/src/zombies/ZombiTanque.java
@@ -0,0 +1,12 @@
+package zombies;
+
+public class ZombiTanque extends Zombi{
+	/**
+	 * Constructor del ZombiTanque,
+	 *  cambiamos los atributos vida y velocidad por 200 y 10
+	 */
+	public ZombiTanque() {
+		super(200,5); //El super coge el constructor de la clase padre
+	}
+
+}
